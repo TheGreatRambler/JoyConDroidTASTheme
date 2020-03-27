4929bb73a140a19e87674200e712f519cc74242b
sidschingis <SijingYou92@gmail.com>
2020-03-27 17:40:09 +0100

    test Baseline

--- public/js/sendInputs.js
+++ public/js/sendInputs.js
@@ -26,6 +26,8 @@ disableMotionControls();
 
 window.inputHandler = function() {
   var start = performance.now();
+  log( ": " + (performance.now() - start));
+  return true;
   if (pauseTAS) {
     // Just to keep it in check
     clearAllInputs();
@@ -79,7 +81,6 @@ window.inputHandler = function() {
     log("TAS is stopped or has finished");
   }
 
-  log(currentScriptParser.frame + ": " + (performance.now() - start));
 
   return true;
 }
