07bf79375f897cb0944e77d4b3c24b32ea65bc47
sidschingis <SijingYou92@gmail.com>
2020-05-13 16:49:23 +0200

    Update Visualizer
    Use the image names  instead of numbers
    Images are now initialized on deman

--- public/js/controllerVisualizer.js
+++ public/js/controllerVisualizer.js
@@ -1,9 +1,5 @@
 // Uses some stuff from TASVis
-// Last one is blank, dont blame me
-var inputsNames = ["KEY_A", "KEY_B", "KEY_X", "KEY_Y", "KEY_L", "KEY_R", "KEY_ZL", "KEY_ZR", "KEY_PLUS", "KEY_MINUS", "KEY_DLEFT", "KEY_DUP", "KEY_DRIGHT", "KEY_DDOWN", "blank"];
-// Adds images that arent keys for now
-var imagesToMake = ["proconbase", "leftstick", "rightstick"].concat(inputsNames);
-var inputImages = [];
+var inputImages = {};
 var imageContainer = document.getElementById("controllerImageContainer");
 
 var currentlyVisible = true;
@@ -21,6 +17,8 @@ function hideController() {
 }
 
 function visible(isVisible, index) {
+  makeImage(index);
+
   if (isVisible) {
     inputImages[index].style.visibility = "visible";
   } else {
@@ -33,31 +31,47 @@ function move(index, x, y) {
   inputImages[index].style.top = y + "px";
 }
 
-function init() {
+function makeImage(key){
+  if (inputImages[key])
+  {
+    return;
+  }
+
   // Every image is sized 1000 by 750
   // Width of screen
   var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-  var currentIndex = 0;
-  imagesToMake.forEach(function(key) {
-    var oImg = document.createElement("img");
-    oImg.setAttribute("src", "./images/controller/" + key + ".png");
-    // Set to existing class
-    oImg.className = "controllerImages";
-    // Its a bit too big otherwise
-    oImg.style.width = w + "px";
-    // Situate image
-    oImg.style.top = 0;
-    oImg.style["z-index"] = currentIndex;
-    // Hide image for now
-    oImg.style.visibility = "hidden";
-    currentIndex++;
-    imageContainer.appendChild(oImg);
-    inputImages.push(oImg);
-  });
+  var oImg = document.createElement("img");
+  oImg.setAttribute("src", "./images/controller/" + key + ".png");
+  // Set to existing class
+  oImg.className = "controllerImages";
+  // Its a bit too big otherwise
+  oImg.style.width = w + "px";
+  // Situate image
+  oImg.style.top = 0;
+
+  switch (key)
+  {
+      case "proconbase":
+      zIndex = 0;
+      break;
+
+    default:
+      zindex = 1;
+      break;
+  }
+
+  oImg.style["z-index"] = zIndex;
+  // Hide image for now
+  oImg.style.visibility = "hidden";
+  imageContainer.appendChild(oImg);
+  inputImages[key] = oImg;
+}
+
+function init() {
   // Make proconbase visible as well as the sticks
-  visible(true, 0);
-  visible(true, 1);
-  visible(true, 2);
+  visible(true, "proconbase");
+  visible(true, "leftstick");
+  visible(true, "rightstick");
 }
 
 // Start
@@ -65,23 +79,6 @@ init();
 
 // Toggle controller visibility
 document.getElementById("showController").onclick = showController;
-
-var keyIndex = {
-  KEY_A: 3,
-  KEY_B: 4,
-  KEY_X: 5,
-  KEY_Y: 6,
-  KEY_L: 7,
-  KEY_R: 8,
-  KEY_ZL: 9,
-  KEY_ZR: 10,
-  KEY_PLUS: 11,
-  KEY_MINUS: 12,
-  KEY_DLEFT: 13,
-  KEY_DUP: 14,
-  KEY_DRIGHT: 15,
-  KEY_DDOWN: 16,
-};
 
 function setControllerVisualizer(inputs) {
 	if (!currentlyVisible)
@@ -92,30 +89,45 @@ function setControllerVisualizer(inputs) {
   if (!inputs) {
     // No inputs this frame
     // Make blank image viewable
-    visible(true, 17);
+    visible(true, "blank");
 		return;
   }
 
   // Set blank as invisible automatically
-  visible(false, 17);
+  visible(false, "blank");
 
-  // May init this some other way, this is good right now
-  var listOfButtons = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+  var listOfButtons = {
+    KEY_A: 0,
+    KEY_B: 0,
+    KEY_X: 0,
+    KEY_Y: 0,
+    KEY_L: 0,
+    KEY_R: 0,
+    KEY_ZL: 0,
+    KEY_ZR: 0,
+    KEY_PLUS: 0,
+    KEY_MINUS: 0,
+    KEY_DLEFT: 0,
+    KEY_DUP: 0,
+    KEY_DRIGHT:0,
+    KEY_DDOWN: 0,
+    KEY_HOME: 0,
+  };
 
   var numActiveButtons = inputs.buttons.length;
   for (var i = 0; i < numActiveButtons; i++) {
-    var keyNum = keyIndex[KEY_INT_ARRAY[inputs.buttons[i]]];
-    visible(true, keyNum);
+    var keyName = KEY_INT_ARRAY[inputs.buttons[i]];
+    visible(true, keyName);
     // Set as already run
-    listOfButtons[keyNum - 3] = -1;
+    listOfButtons[keyName] = 1;
   }
 
-  listOfButtons.forEach(function(button) {
-    if (button !== -1) {
-      // Hide buttons that did not run this frame
-      visible(false, button);
-    }
-  });
+  // Hide buttons that did not run this frame
+  for (var button in listOfButtons) {
+      if (listOfButtons[button] == 0) {
+        visible(false, button);
+      }
+  }
 
   // Joysticks
   // Left stick
@@ -126,10 +138,10 @@ function setControllerVisualizer(inputs) {
     var xOffset = LX / 4;
     // Y is opposite
     var yOffset = LY / -4;
-    move(1, xOffset, yOffset);
+    move("leftstick", xOffset, yOffset);
   } else {
     // Reset
-    move(1, 0, 0);
+    move("leftstick", 0, 0);
   }
 
   // Right stick
@@ -139,8 +151,8 @@ function setControllerVisualizer(inputs) {
     // At least one needs moving
     var xOffset = RX / 4;
     var yOffset = RY / -4;
-    move(2, xOffset, yOffset);
+    move("rightstick", xOffset, yOffset);
   } else {
-    move(2, 0, 0);
+    move("rightstick", 0, 0);
   }
 }
