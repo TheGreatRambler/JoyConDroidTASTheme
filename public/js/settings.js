430ea79055818062243ba132d7f68e9507938b63
sidschingis <SijingYou92@gmail.com>
2020-05-24 16:50:00 +0200

    Use class instead of inline CSS

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,14 +1,5 @@
-var NOT_USED_X = "-300%";
-var NOT_USED_Y = "0%";
-
 var TAS_WINDOW = document.getElementById("TASContainer");
-var TAS_WINDOW_X = "-0.25%";
-var TAS_WINDOW_Y = "-0.25%";
-
 var SETTINGS_WINDOW = document.getElementById("SettingsContainer");
-var SETTINGS_WINDOW_X = "-0.25%";
-var SETTINGS_WINDOW_Y = "-0.25%";
-
 // Settings for entire program
 /*
   Parsing style represents way in which inputs are parsed
@@ -40,34 +31,26 @@ var PARSER_FIRST_VERSION = 0;
 function openTASWindow() {
   closeSettingsWindow();
   // Open only the TAS window
-  TAS_WINDOW.style.visibility = "visible";
-  TAS_WINDOW.style.left = TAS_WINDOW_X;
-  TAS_WINDOW.style.top = TAS_WINDOW_Y;
+  TAS_WINDOW.classList.remove("hidden");
   // CSS animation will automatically be added
 }
 
 function closeTASWindow() {
   // Open close the TAS window
-  TAS_WINDOW.style.visibility = "hidden";
-  TAS_WINDOW.style.left = NOT_USED_X;
-  TAS_WINDOW.style.top = NOT_USED_Y;
+  TAS_WINDOW.classList.add("hidden");
   // CSS animation will automatically be added
 }
 
 function openSettingsWindow() {
   closeTASWindow();
   // Open only the TAS window
-  SETTINGS_WINDOW.style.visibility = "visible";
-  SETTINGS_WINDOW.style.left = SETTINGS_WINDOW_X;
-  SETTINGS_WINDOW.style.top = SETTINGS_WINDOW_Y;
+  SETTINGS_WINDOW.classList.remove("hidden");
   // CSS animation will automatically be added
 }
 
 function closeSettingsWindow() {
   // Open close the TAS window
-  SETTINGS_WINDOW.style.visibility = "hidden";
-  SETTINGS_WINDOW.style.left = NOT_USED_X;
-  SETTINGS_WINDOW.style.top = NOT_USED_Y;
+  SETTINGS_WINDOW.classList.add("hidden");
   // CSS animation will automatically be added
 }
 
